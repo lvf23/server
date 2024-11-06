ef371780caf662256d623a231e5d34879ef7c477
Lucas Vendramini <lvfx23@gmail.com>
2024-11-05 21:00:04 -0300

    adjust paths for module-alias paths

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ require("module-alias/register");
 const { loadPlugins } = require("@root/submodules/hooks/plugins");
 const { runMods } = require("@root/submodules/hooks/mods");
 
-const { fetchPlugins } = require("./utils/plugins");
-const { startServer, createRoute, setRoutePrefix } = require("./utils/server");
+const { fetchPlugins } = require("@root/utils/plugins");
+const {
+  startServer,
+  createRoute,
+  setRoutePrefix,
+} = require("@root/utils/server");
 
 const { loadJobs } = require("@root/utils/jobs");
 const { loadMods } = require("@root/utils/mods");
